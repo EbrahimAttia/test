3be13da7f9db3bc9eda4025aabe330333987a203
EbrahimAttia <eattia@studenti.apuliadigitalmaker.it>
2025-12-09 12:25:27 +0100

    Add project files.

---
+++ test/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="test._Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Database Connection Test</h2>
+    <p>Click the button below to test the connection to your MySQL database via XAMPP.</p>
+   
+    <div style="margin: 20px 0;">
+        <asp:Button ID="btnTest" runat="server" Text="Test MySQL Connection" OnClick="BtnTest_Click" CssClass="btn btn-primary" />
+    </div>
+
+    <div class="alert alert-info">
+        <p><strong>Result:</strong></p>
+        <asp:Label ID="lblResult" runat="server" Text="Waiting to connect..."></asp:Label>
+    </div>
+
+    <div style="margin-top:20px">
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true"
+        CssClass="table table-bordered table-striped"></asp:GridView>
+</div>
+
+
+</asp:Content>
---
+++ test/Default.aspx.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient; // **Needed for MySQL commands**
+using MySqlX.XDevAPI.Relational;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace test
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnTest_Click(object sender, EventArgs e)
+        {
+            // 1. Get the connection string from Web.config
+            // This pulls the string we named "TicketingDb"
+            string connString = ConfigurationManager.ConnectionStrings["TicketingDb"].ConnectionString;
+
+            // 2. Create the connection object and ensure it is properly disposed (using statement)
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                try
+                {
+                    // 3. Try to open the connection
+                    conn.Open();
+
+                    // Connection was successful!
+                    lblResult.Text = "Success! Connected to MySQL. XAMPP is running and the database details are correct.";
+                    lblResult.ForeColor = System.Drawing.Color.Green;
+
+                    // ---- RUN A QUERY HERE ----
+                    string query = "SELECT * FROM utente ";
+                    // Replace your_table_name with any table that exists in your MySQL DB
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // Bind to GridView
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    // Connection failed. Show the specific error message.
+                    lblResult.Text = "Error connecting to MySQL: " + ex.Message;
+                    lblResult.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+    }
+}
+
+
+        
+    
+
+
